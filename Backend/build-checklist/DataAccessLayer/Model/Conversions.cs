b22f42790dd71b8b94cbdf2bbe8dff0536520cbd
Rajesh G <rajesh@carestack.com>
2020-11-03 15:26:16 +0530

    before creating tables

---
+++ Backend/build-checklist/DataAccessLayer/Model/Conversions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Model
+{
+    public class Conversions
+    {
+        public int ConversionId { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string ConversionName { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string ConversionDescription { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Member1 { get; set; }
+        [MaxLength(255)]
+        public string Member2 { get; set; }
+        [MaxLength(255)]
+        public string Member3 { get; set; }
+        [MaxLength(255)]
+        public string Member4 { get; set; }
+        public bool CompletedStatus { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ Backend/build-checklist/DataAccessLayer/Model/Users.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Model
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Password { get; set; }
+        public bool Lead { get; set; }
+        public bool Admin { get; set; }
+        [MaxLength(255)]
+        public string role { get; set; }
+    }
+}
