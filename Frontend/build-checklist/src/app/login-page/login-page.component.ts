7f19ff77778fd8b13761c8c9a9c32e964826ba0d
Rajesh G <rajesh@carestack.com>
2020-11-03 10:38:34 +0530

    validation

--- Frontend/build-checklist/src/app/login-page/login-page.component.ts
+++ Frontend/build-checklist/src/app/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -11,7 +11,7 @@ export class LoginPageComponent implements OnInit {
 
   myForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.myForm = new FormGroup({
@@ -19,6 +19,13 @@ export class LoginPageComponent implements OnInit {
       email: new FormControl(''),
       password: new FormControl('')
     });
+
+    this.myForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.pattern('[a-z0-9.@]*')]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+
   }
 
   onSubmit(form: FormGroup) {
