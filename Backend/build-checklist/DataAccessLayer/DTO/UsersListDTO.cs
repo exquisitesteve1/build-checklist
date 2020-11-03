e887eb72219f3eaed2320a40f107c09408a29515
Rajesh G <rajesh@carestack.com>
2020-11-03 23:31:18 +0530

    add DTOs

---
+++ Backend/build-checklist/DataAccessLayer/DTO/UsersListDTO.cs
@@ -0,0 +1,19 @@
+﻿using DataAccessLayer.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DTO
+{
+    public class UsersListDTO
+    {
+        public int UserId { get; set;}
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public bool Lead { get; set; }
+        public bool Admin { get; set; }
+        public string Role { get; set; }
+    }
+}
